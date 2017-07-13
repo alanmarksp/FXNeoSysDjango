52f5fef0a57ef15d71f0dbc7ac0f4602c334af7c
Alan Mark <alanmarksp@gmail.com>
2017-07-13 09:35:05 +0200

    Implement periodic task

--- quotes/models.py
+++ quotes/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from django.db.models import Max
+
+
+class QuotesManager(models.Manager):
+
+    def latest_quotes(self):
+        quotes = self.model.objects.none()
+        for symbol in self.model.SYMBOLS:
+            q = self.model.objects.filter(symbol=symbol[0])
+            latest_date = q.aggregate(datetime=Max('datetime'))
+            quotes = quotes | q.filter(datetime=latest_date['datetime'])
+        return quotes
+
 
 class Quote(models.Model):
-    pass
+    EURUSD = 'EURUSD'
+    GBPUSD = 'GBPUSD'
+    USDJPY = 'USDJPY'
+    USDCHF = 'USDCHF'
+    SYMBOLS = (
+        (EURUSD, 'Euro vs US Dollar'),
+        (GBPUSD, 'Great Britain vs US Dollar'),
+        (USDJPY, 'US Dollar vs Japanese Yen'),
+        (USDCHF, 'US Dollar vs Swiss Franc')
+    )
+    symbol = models.CharField(
+        max_length=6,
+        choices=SYMBOLS
+    )
+    bid_price = models.DecimalField(max_digits=10, decimal_places=4)
+    ask_price = models.DecimalField(max_digits=10, decimal_places=4)
+    datetime = models.DateTimeField(auto_now_add=True)
+    objects = QuotesManager()
--- trading_accounts/models.py
+++ trading_accounts/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from traders.models import Trader
 
 
 class TradingAccount(models.Model):
     funds = models.DecimalField(name='funds', max_digits=12, decimal_places=2, default=10000.00)
-    realized_profit = models.DecimalField(name='realized profit', max_digits=10, decimal_places=2, default=0)
+    realized_profit = models.DecimalField(name='realized_profit', max_digits=10, decimal_places=2, default=0)
     owner = models.ForeignKey(Trader, name='owner', related_name='trading_accounts')
+
+
+@receiver(post_save, sender=TradingAccount)
+def save_trading_account(sender, instance, **kwargs):
+    trader = instance.owner
+    if not trader.selected_trading_account:
+        trader.selected_trading_account = instance
+        trader.save()
